380f5471025b22e596ee6ebc2f78545dc777e898
smlevorse <smlevorse@msn.com>
2018-10-11 09:16:59 -0400

    Now reads maze.txt

--- STICK/Assets/Scripts/SceneManager.cs
+++ STICK/Assets/Scripts/SceneManager.cs
@@ -5,31 +5,17 @@ using System.Linq;
 
 public class SceneManager : MonoBehaviour {
 
-    private static string mazeText =
-        "01000000000000\r\n" +
-        "01111100000000\r\n" +
-        "01000111100000\r\n" +
-        "01000000111110\r\n" +
-        "01000000100000\r\n" +
-        "01111111100000\r\n" +
-        "00010000000000\r\n" +
-        "00011110000000\r\n" +
-        "00000100000000\r\n" +
-        "00111111111100\r\n" +
-        "00100000001000\r\n" +
-        "00111111111000\r\n" +
-        "00000000001110\r\n" +
-        "00000000001000";
-
     public GameObject wallBlockPrefab;
     public GameObject player;
     public GameObject mainCamera;
+    public TextAsset mazeFile;
 
     // Use this for initialization
 	void Start () {
         // Build the wall!
+        
         float wallWidth = wallBlockPrefab.transform.lossyScale.x;
-        int[,] maze = readMaze(mazeText);
+        int[,] maze = readMaze(mazeFile.text);
         Vector3 mazeOffset = new Vector3(-maze.GetLength(0) * wallWidth / 2, -maze.GetLength(1) * wallWidth / 2, 9.5f);
         for (int i = 0; i < maze.GetLength(0); i++)
         {
