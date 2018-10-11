d06df7990885135a1a5bb91331cf4c96aeff8707
Unknown <deb2610@g.rit.edu>
2018-10-11 02:11:54 -0400

    Added: Trees, Ground, and Lighting
    also did post processing, and added in the beta music

---
+++ STICK/Assets/Scripts/Treeinator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treeinator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Vector3 randRotation = new Vector3(transform.rotation.x,  transform.rotation.y, Random.Range(0, 360));
+        gameObject.transform.rotation = Quaternion.Euler(randRotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
