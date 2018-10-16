813f24295b9507e51c6dc0734593cd2a80740a5a
smlevorse <smlevorse@msn.com>
2018-10-15 20:07:16 -0400

    Added check for player object

---
+++ STICK/Assets/Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+        {
+            // TODO: End the game
+        }
+    }
+}
